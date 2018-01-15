01ccd00b3d7ae8822ac207d10c1ea7832080732a
Aayushi Modi <modi.aayu@gmail.com>
2018-01-14 19:23:03 -0800

    Revert the branch to change 5634656cb314e5ce73457174922d4f1b038e0879 (Added arcade drive. Fixed TalonSRX to WPI_TalonSRX)

--- src/org/usfirst/frc/team2976/robot/subsystems/RobotArm.java
+++ src/org/usfirst/frc/team2976/robot/subsystems/RobotArm.java
@@ -3,46 +3,30 @@ package org.usfirst.frc.team2976.robot.subsystems;
 import org.usfirst.frc.team2976.robot.RobotMap;
 
 import com.ctre.CANTalon;
-<<<<<<< HEAD
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-=======
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
->>>>>>> master
 
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
-public class RobotArm extends PIDSubsystem {
+public class RobotArm extends Subsystem {
 
-    public RobotArm(double p, double i, double d) {
-		super(p, i, d);
-	}
-	// Put methods for controlling this subsystem
+    // Put methods for controlling this subsystem
     // here. Call these from Commands.
-	WPI_TalonSRX robotMotorArm= new WPI_TalonSRX(RobotMap.robotArmMotorPort); 
+	TalonSRX robotMotorArm= new TalonSRX(RobotMap.robotArmMotorPort); 
 	//the limit switch that is pressed when the arm goes up
 	DigitalInput robotArmLimitSwitchUp= new DigitalInput(RobotMap.robotArmLimitSwitchPortUp);
 	//the limit switch that is pressed when the arm goes down
 	DigitalInput robotArmLimitSwitchDown= new DigitalInput(RobotMap.robotArmLimitSwitchPortDown);
-	
-	Encoder armEncoder = new Encoder(0,1); //TODO FIX PORTS
-	
 	public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
 	public void setPower(double p){
-<<<<<<< HEAD
-		//robotMotorArm.set(p);
-=======
 		robotMotorArm.set(ControlMode.PercentOutput, p);
->>>>>>> master
 	}
 	
 	public boolean getArmUpLimitSwitch(){
@@ -52,28 +36,6 @@ public class RobotArm extends PIDSubsystem {
 	public boolean getArmDownLimitSwitch(){
 		boolean armSwitchState = robotArmLimitSwitchDown.get();
 		return armSwitchState;
-	}
-	@Override
-	protected double returnPIDInput() {
-		// TODO Auto-generated method stub
-		return armEncoder.get();
-	}
-	
-	@Override
-	protected void usePIDOutput(double output) { 
-		
-		if (getArmUpLimitSwitch() == true && output > 0) { //up > 0
-			robotMotorArm.set(robotMotorArm.getControlMode(),1); //some small power so the arm doesn't fall down
-		} else if (getArmUpLimitSwitch() == true && 0 > output) { //up < 0
-			robotMotorArm.set(robotMotorArm.getControlMode(),output); //give output power
-		} else if (getArmDownLimitSwitch() == true && 0 < output) { //down < 0
-			robotMotorArm.set(robotMotorArm.getControlMode(), 0); //no power
-		} else if (getArmDownLimitSwitch() == true && 0 > output) { //down > 0
-			robotMotorArm.set(robotMotorArm.getControlMode(), output); //give output power
-		} else {
-			robotMotorArm.set(robotMotorArm.getControlMode(), output); //give output power
-		}
-	 
 	}
 }
 
